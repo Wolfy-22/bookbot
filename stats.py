401fe9a3edea6712046a87a6c88e6e841d7ad68f
Wolfy22 <Bubblarr@Gmail.com>
2025-06-02 09:52:49 -0400

    updated stats.py

--- stats.py
+++ stats.py
@@ -12,32 +12,15 @@ def char_number(file_contents):
     return file_letter_count
 
 def sorted_dict(dict):
-    dict_list = [{"char":"a", "num":dict["a"]}, 
-                 {"char":"b", "num":dict["b"]}, 
-                 {"char":"c", "num":dict["c"]}, 
-                 {"char":"d", "num":dict["d"]}, 
-                 {"char":"e", "num":dict["e"]},  
-                 {"char":"g", "num":dict["g"]}, 
-                 {"char":"h", "num":dict["h"]}, 
-                 {"char":"i", "num":dict["i"]}, 
-                 {"char":"j", "num":dict["j"]}, 
-                 {"char":"k", "num":dict["k"]}, 
-                 {"char":"l", "num":dict["l"]}, 
-                 {"char":"m", "num":dict["m"]}, 
-                 {"char":"n", "num":dict["n"]}, 
-                 {"char":"o", "num":dict["o"]}, 
-                 {"char":"p", "num":dict["p"]}, 
-                 {"char":"q", "num":dict["q"]}, 
-                 {"char":"r", "num":dict["r"]}, 
-                 {"char":"s", "num":dict["s"]}, 
-                 {"char":"t", "num":dict["t"]}, 
-                 {"char":"u", "num":dict["u"]}, 
-                 {"char":"v", "num":dict["v"]}, 
-                 {"char":"w", "num":dict["w"]}, 
-                 {"char":"x", "num":dict["x"]}, 
-                 {"char":"y", "num":dict["y"]},
-                 {"char":"z", "num":dict["z"]}]
-    dict_list.sort(reverse=True)
+    dict_list = []
+    alphabet = {"a", "b", "c", "d", "e", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+    
+    for i in alphabet:
+        dict_list.append({"char":i, "num":dict[i]})
+
+    dict_list.sort(reverse=True, key=sort_on)
     return dict_list
 
+def sort_on(dict):
+    return dict["num"]
     
