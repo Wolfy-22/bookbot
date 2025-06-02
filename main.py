d6b3337c79422ce857cbebdc592427c7ad3d4355
Wolfy22 <Bubblarr@Gmail.com>
2025-06-02 10:28:11 -0400

    Changed bookbot to work with any book

--- main.py
+++ main.py
@@ -8,13 +8,35 @@ def get_book_text(File_path):
 
 
 def main():
-    book_length = get_book_length(get_book_text("books/frankenstein.txt"))
-    print(f"{book_length} words found in the document")
+    import sys
+    
 
-    char_count = char_number(get_book_text("books/frankenstein.txt"))
-    print(char_count)
+    try:
+        book_length = get_book_length(get_book_text(sys.argv[1]))
+    except Exception:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+
+    char_count = char_number(get_book_text(sys.argv[1]))
 
     dict_sorted = sorted_dict(char_count)
-    print(dict_sorted)
+
+    print("============ BOOKBOT ============")
+    print("")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("")
+    print("----------- Word Count ----------")
+    print("")
+    print(f"Found {book_length} total words")
+    print("")
+    print("-------- Character Count --------")
+    print("")
+
+    for dict in dict_sorted:
+        print(f"{dict["char"]}: {dict["num"]} ")
+
+    print("")
+    print("============= END ==============")
 
 main()
